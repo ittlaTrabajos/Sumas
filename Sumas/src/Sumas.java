65bf22d868bae1e3c70188be6209962e7e28e862
ittlaTrabajos <L24251117@tlalnepantla.tecnm.mx>
2024-10-08 18:43:29 -0600

    Add files via upload

---
+++ Sumas/src/Sumas.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Sumas {
+
+	public static void main(String[] args) {
+		Scanner entrada =new Scanner(System.in); 
+		int a,b,c; // los espacios para la suma y los dos valores que se nos daran
+		System.out.println("Ingresa dos valores"); //le pedimos al usuario en la pantalla que ponga dos valores
+		a=entrada.nextInt(); //el usuario ingresa un numero 
+		b=entrada.nextInt(); //el usuario ingresa un numero 
+		c=(a+b); // se realiza la suma con los numeros dados por el usuario
+		System.out.println("El resultado es "+c); // mostramos los resultados en pantalla para que los vea el usuario
+		
+		/*Rodriguez Iñiguez Ivonne
+		 * No. control 24251117
+		 * 55 6451 0855
+		 */
+	}
+
+}
