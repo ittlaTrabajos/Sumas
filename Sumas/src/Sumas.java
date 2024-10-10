83daae66cfa79b26abd54f2808d4e1292249f0aa
ittlaTrabajos <L24251117@tlalnepantla.tecnm.mx>
2024-10-10 06:50:43 -0600

    Update Sumas.java

--- Sumas/src/Sumas.java
+++ Sumas/src/Sumas.java
@@ -4,10 +4,10 @@ public class Sumas {
 
 	public static void main(String[] args) {
 		Scanner entrada =new Scanner(System.in); 
-		int a,b,c; // los espacios para la suma y los dos valores que se nos daran
+		double a,b,c; // los espacios para la suma y los dos valores que se nos daran
 		System.out.println("Ingresa dos valores"); //le pedimos al usuario en la pantalla que ponga dos valores
-		a=entrada.nextInt(); //el usuario ingresa un numero 
-		b=entrada.nextInt(); //el usuario ingresa un numero 
+		a=entrada.nextDouble(); //el usuario ingresa un numero 
+		b=entrada.nextDouble(); //el usuario ingresa un numero 
 		c=(a+b); // se realiza la suma con los numeros dados por el usuario
 		System.out.println("El resultado es "+c); // mostramos los resultados en pantalla para que los vea el usuario
 		
